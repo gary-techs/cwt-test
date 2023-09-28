2ba0d07cd73c3fbd9c73d9a10435093bb44c14a4
Girish Sharma <girish@wifitics.com>
2023-09-28 18:10:13 +0530

    apis added

--- api.py
+++ api.py
@@ -1,6 +1,4 @@
-import os
-import json
-import requests
+import os, re, json, requests
 from dotenv import load_dotenv
 from helper import print_success, print_info
 
@@ -8,17 +6,16 @@ from helper import print_success, print_info
 load_dotenv()
 
 def make_request(endpoint, method="GET", data=None):
-    base_url = os.environ['BASE_URL']
-    version = os.environ["VERSION"]
-    
     # Set up headers
     headers = {
-    "Authorization": f"Basic {os.environ['BEARER_TOKEN']}", 
-    "CwtLicenseKey": f"{os.environ['CWT_LICENSE_KEY']}"
+        "Authorization": f"Basic {os.environ['BEARER_TOKEN']}",
+        "CwtLicenseKey": f"{os.environ['CWT_LICENSE_KEY']}",
     }
 
-    url = f"{base_url}{version}{endpoint}"
-    print_info("\nFetching " + url)
+    url = f"{os.environ['BASE_URL']}{os.environ['VERSION']}{endpoint}"
+
+    print_info(f"\nFetching  {url}")
+
     if method == "GET":
         response = requests.get(url, headers=headers)
     elif method == "POST":
@@ -29,31 +26,84 @@ def make_request(endpoint, method="GET", data=None):
         raise ValueError("Invalid HTTP method")
     return response
 
+
 def get_status():
     endpoint = "/Status"
     response = make_request(endpoint, method="GET")
     if response.status_code == 200:
         result = json.loads(response.text)
-        if result['status'] == "OK": 
+        if result["status"] == "OK":
             print_success(f"Success - GET {endpoint}")
 
+
 def get_all_products():
-    first_product_id=None
+    first_product = None
     endpoint = "/Products"
     response = make_request(endpoint, method="GET")
     if response.status_code == 200:
         products = json.loads(response.text)
         print_success(f"Success - GET {endpoint}")
-        first_product_id = products[0]['id']
+        first_product = products[0]
         for idx, product in enumerate(products):
-            print_success(str(idx+1)+". "+ product['title'])
-    return first_product_id
+            print_success(f"{str(idx + 1)}. {product['title']}")
+    return first_product
 
-def get_product_info(first_product_id):
-    endpoint = "/Products/"+str(first_product_id)
+def get_product_info(product_id):
+    endpoint = f"/Products/{product_id}"
     response = make_request(endpoint, method="GET")
     if response.status_code == 200:
         products = json.loads(response.text)
         print_success(f"Success - GET {endpoint}")
-        print_success("Title:- " + products['title'])
-        print_success("Lines Of Code:- " + str(products['linesOfCode']))
+        print_success(f"Title:- {products['title']}")
+        print_success(f"Lines Of Code:- {str(products['linesOfCode'])}")
+
+def save_engineering_report_by_scan_id(scan_id):
+    endpoint = f"/reports/Engineering/{str(scan_id)}"
+    response = make_request(endpoint, method="GET")
+    if response.status_code == 200:
+        print_success(f"Success - GET {endpoint}")
+        # Extract the filename from the Content-Disposition header using regex
+        match = re.search(
+            r"filename=(.*?)(?:;|$)", response.headers.get("content-disposition")
+        )
+        filename = f"{os.environ['DOWNLOADED_FILES_PATH']}{match.group(1)}"
+
+        # Save the file to disk
+        with open(filename, "wb") as file:
+            file.write(response.content)
+
+        print_success(f"File saved as {filename}")
+
+def save_executive_report_by_scan_id(scan_id):
+    endpoint = f"/reports/Executive/{str(scan_id)}"
+    response = make_request(endpoint, method="GET")
+    if response.status_code == 200:
+        print_success(f"Success - GET {endpoint}")
+        # Extract the filename from the Content-Disposition header using regex
+        match = re.search(
+            r"filename=(.*?)(?:;|$)", response.headers.get("content-disposition")
+        )
+        filename = f"{os.environ['DOWNLOADED_FILES_PATH']}{match.group(1)}"
+
+        # Save the file to disk
+        with open(filename, "wb") as file:
+            file.write(response.content)
+
+        print_success(f"File saved as {filename}")
+
+def save_executive_details_report_by_scan_id(scan_id):
+    endpoint = f"/reports/Executive/details/{str(scan_id)}"
+    response = make_request(endpoint, method="GET")
+    if response.status_code == 200:
+        print_success(f"Success - GET {endpoint}")
+        # Extract the filename from the Content-Disposition header using regex
+        match = re.search(
+            r"filename=(.*?)(?:;|$)", response.headers.get("content-disposition")
+        )
+        filename = f"{os.environ['DOWNLOADED_FILES_PATH']}{match.group(1)}"
+
+        # Save the file to disk
+        with open(filename, "wb") as file:
+            file.write(response.content)
+
+        print_success(f"File saved as {filename}")
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from api import get_status, get_all_products, get_product_info
+from api import (
+    get_status,
+    get_all_products,
+    get_product_info,
+    save_executive_report_by_scan_id,
+    save_executive_details_report_by_scan_id,
+    save_engineering_report_by_scan_id
+)
 
-#All the three endpoints are tested here
+
+# All the three endpoints are tested here
 def main():
     get_status()
-    first_product_id = get_all_products()
-    get_product_info(first_product_id)
+    first_product = get_all_products()
+    get_product_info(first_product["id"])
+    save_executive_report_by_scan_id(first_product["scans"][0]["id"])
+    save_executive_details_report_by_scan_id(first_product["scans"][0]["id"])
+    save_engineering_report_by_scan_id(first_product["scans"][0]["id"])
+
 
 if __name__ == "__main__":
     main()

55f03976a914e233b49dac7554b33cc032fa8ebb
Girish Sharma <girish@wifitics.com>
2023-09-28 18:40:58 +0530

    fixes

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ from api import (
     save_engineering_report_by_scan_id
 )
 
-
 # All the three endpoints are tested here
 def main():
     get_status()
+    
     first_product = get_all_products()
     get_product_info(first_product["id"])
-    save_executive_report_by_scan_id(first_product["scans"][0]["id"])
-    save_executive_details_report_by_scan_id(first_product["scans"][0]["id"])
-    save_engineering_report_by_scan_id(first_product["scans"][0]["id"])
+
+    scan_id = first_product["scans"][0]["id"]
+    
+    save_executive_report_by_scan_id(scan_id)
+    save_executive_details_report_by_scan_id(scan_id)
+    save_engineering_report_by_scan_id(scan_id)
 
 
 if __name__ == "__main__":
